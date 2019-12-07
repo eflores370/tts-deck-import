760f698246378a8cc36fe5b865d535c68a354468
andywong <medecinqui@gmail.com>
2019-12-06 21:02:31 -0800

    fix gitignore, iamge stitching functionality, downloads card front and back for transform cards

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 # andy wong
 
 import json
+from PIL import Image
 from functions import *
 
 # step 1: process decklist
@@ -12,29 +13,26 @@ from functions import *
 
 def main():
 
-    cardNames, cardFrequency = processDecklist("testdecklist.txt")
-    print(cardNames)
-    print(cardFrequency)
+    deckName = "lordwimdy"
+    cardNames, cardFrequency = processDecklist("lordwimdy.txt")
     cardData, tokenData = cardCollection(cardNames)
     if cardData is None:
         print("end")
         return
     downloadImages(cardData)
     downloadImages(tokenData, token=True)
-    stitchImages()
-    generateJSON(cardData)
+    stitchImages(deckName, len(cardData), len(tokenData))
+    generateJSON(cardData, tokenData)
     print("deck generated")
 
 def processDecklist(deckName):
 
     with open("decklists/" + deckName, "r") as deckFile:
         print("opening " + deckName)
-        zippedCards = [tuple(line.rstrip().split(maxsplit=1)) for line in deckFile if line]
+        zippedCards = [tuple(line.rstrip().split(maxsplit=1)) for line in deckFile if line.rstrip()]
         cardFrequencyStr, cardNames = zip(*zippedCards)
         cardFrequency = [int(str) for str in cardFrequencyStr]
     return cardNames, cardFrequency
-
-    return ""
 
 def cardCollection(cardNames):
 
@@ -57,17 +55,42 @@ def cardCollection(cardNames):
     return (cardData, tokenData)
 
 def downloadImages(cardData, token=False):
+
     append = "t" if token else ""
     for x, cardInfo in enumerate(cardData):
-        getpost(
-            url = cardInfo["image_uris"]["normal"],
-            filename = ".temp/" + append + str(x) + ".jpg"
-        )
+        # if it's a transform card
+        if "card_faces" in cardInfo:
+            getpost(
+                url = cardInfo["card_faces"][0]["image_uris"]["normal"],
+                filename = ".temp/" + append + str(x) + ".jpg"
+            )
+            getpost(
+                url = cardInfo["card_faces"][1]["image_uris"]["normal"],
+                filename = ".temp/b" + append + str(x) + ".jpg"
+            )
+        else:
+            getpost(
+                url = cardInfo["image_uris"]["normal"],
+                filename = ".temp/" + append + str(x) + ".jpg"
+            )
 
-def stitchImages():
-    return ""
+def stitchImages(deckName, numCards, numTokens):
 
-def generateJSON(cardData):
+    # stitch card images into a 5x5 grid
+    imageNames = [".temp/" + str(num) + ".jpg" for num in range(numCards)]
+    chunks = chunkify(imageNames, 24)
+    for i, chunk in enumerate(chunks):
+        images = [Image.open(imageName) for imageName in chunk]
+        width, height = images[0].size
+        stitchedImage = Image.new("RGB", (5*width, 5*height))
+        for j, image in enumerate(images):
+            stitchedImage.paste(image, (j%5*width, int(j/5)*(height)))
+        stitchedImage.save(".temp/_" + str(i) + ".jpg")
+
+    # stitch token images
+
+
+def generateJSON(cardData, tokenData):
     cardNames = [cardData[x]["name"] for x in range(len(cardData))]
     return ""
 
