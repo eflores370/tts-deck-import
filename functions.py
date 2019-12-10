3e4282af76c96319b5ad3874c8ffb33c6c3960c8
andy w <medecinqui@gmail.com>
2019-12-09 23:20:59 -0800

    added json creation code, first test of project in TTS

--- functions.py
+++ functions.py
@@ -4,6 +4,18 @@
 import requests
 import time
 import json
+from math import ceil
+from PIL import Image
+
+# collection defines
+collectionAllowable = 75
+collectionURL = "https://api.scryfall.com/cards/collection"
+collectionHeaders = {"Content-Type": "application/JSON"}
+
+# stitched image stats
+xDim = 5
+yDim = 5
+maxCards = xDim * yDim - 1
 
 def getpost(url, data = None, headers = None, filename = None):
 
@@ -18,18 +30,13 @@ def getpost(url, data = None, headers = None, filename = None):
         with open(filename, "wb") as file:
             file.write(response.content)
 
-def chunkify(arr, size):
+def chunkify(arr, size = maxCards):
 
     return [arr[x:x + size] for x in range(0, len(arr), size)]
 
 def getbyfield(field, identifiers):
 
-    # useful defines
-    collectionAllowable = 75
-    collectionURL = "https://api.scryfall.com/cards/collection"
-    collectionHeaders = {"Content-Type": "application/JSON"}
-
-    chunks = chunkify(identifiers, 75)
+    chunks = chunkify(identifiers, collectionAllowable)
     cardData = []
     failures = []
 
@@ -51,3 +58,65 @@ def getbyfield(field, identifiers):
         failures += collection["not_found"]
 
     return (cardData, failures)
+
+def stitcher(deckName, ids, prefix = ""):
+
+    imageNames = [".temp/" + prefix + str(num) + ".jpg" for num in ids]
+    chunks = chunkify(imageNames, maxCards)
+    for i, chunk in enumerate(chunks):
+        images = [Image.open(imageName) for imageName in chunk]
+        width, height = images[0].size
+        cols, rows = stitchDimensions(len(images))
+        stitchedImage = Image.new("RGB", (cols*width, rows*height))
+        for j, image in enumerate(images):
+            stitchedImage.paste(image, (j%cols*width, int(j/cols)*(height)))
+        stitchedImage.save("images/" + deckName + "_" + prefix + str(i) + ".jpg")
+
+# calculate size of stitcher sheet given number of cards (<=limX*limxY-1)
+def stitchDimensions(num, limX=xDim, limY=yDim):
+    columns = num + 1 if num + 1 < limX else limX
+    rows = ceil((num + 1)/limX)
+    if rows > limY: raise Exception("too many cards to fit on sheet")
+    return (columns, rows)
+
+def transformObject(posX = 0, posY = 0, posZ = 0, rotX = 0, rotY = 180,
+    rotZ = 180, scaleX = 1, scaleY = 1, scaleZ = 1):
+    return {
+        "posX": posX,
+        "posY": posY,
+        "posZ": posZ,
+        "rotX": rotX,
+        "rotY": rotY,
+        "rotZ": rotZ,
+        "scaleX": scaleX,
+        "scaleY": scaleY,
+        "scaleZ": scaleZ
+    }
+
+def cardObject(nickname, cardID, name = "Card", transform = transformObject()):
+    return {
+        "Name": name,
+        "Nickname": nickname,
+        "Transform": transform,
+        "CardID": cardID
+    }
+
+def imageObject(numWidth, numHeight, faceURL, backURL, uniqueBack = None):
+    if uniqueBack is None:
+        return {
+            "NumWidth": numWidth,
+            "NumHeight": numHeight,
+            "FaceURL": faceURL,
+            "BackURL": backURL
+        }
+    else:
+        return {
+            "NumWidth": numWidth,
+            "NumHeight": numHeight,
+            "FaceURL": "https://www.brickbolt.com/tts-deck-import/" + faceURL,
+            "BackURL": "https://www.brickbolt.com/tts-deck-import/" + backURL,
+            "UniqueBack": uniqueBack
+        }
+
+def cardIDGen(num, start = 0):
+    return start + 100 * (1 + int(num/24)) + (num%24)
--- script.py
+++ script.py
@@ -2,7 +2,7 @@
 # andy wong
 
 import json
-from PIL import Image
+from shutil import copyfile
 from functions import *
 
 # step 1: process decklist
@@ -13,16 +13,16 @@ from functions import *
 
 def main():
 
-    deckName = "lordwimdy"
-    cardNames, cardFrequency = processDecklist("lordwimdy.txt")
+    deckName = "thrasiossmash"
+    cardNames, cardFrequency = processDecklist("thrasiossmash.txt")
     cardData, tokenData = cardCollection(cardNames)
     if cardData is None:
         print("end")
         return
-    downloadImages(cardData)
-    downloadImages(tokenData, token=True)
-    stitchImages(deckName, len(cardData), len(tokenData))
-    generateJSON(cardData, tokenData)
+    flipCardNums = downloadImages(cardData)
+    flipCardNums += downloadImages(tokenData, token=True)
+    stitchImages(deckName, len(cardData), len(tokenData), flipCardNums)
+    generateJSON(deckName, cardData, cardFrequency, tokenData, flipCardNums)
     print("deck generated")
 
 def processDecklist(deckName):
@@ -57,41 +57,152 @@ def cardCollection(cardNames):
 def downloadImages(cardData, token=False):
 
     append = "t" if token else ""
+    flipCardNums = []
     for x, cardInfo in enumerate(cardData):
         # if it's a transform card
         if "card_faces" in cardInfo:
+            flipCardNums += [x]
             getpost(
                 url = cardInfo["card_faces"][0]["image_uris"]["normal"],
                 filename = ".temp/" + append + str(x) + ".jpg"
             )
+            # make a flip card copy too
+            copyfile(".temp/" + append + str(x) + ".jpg", ".temp/tf" + append + str(x) + ".jpg")
             getpost(
                 url = cardInfo["card_faces"][1]["image_uris"]["normal"],
-                filename = ".temp/b" + append + str(x) + ".jpg"
+                filename = ".temp/tb" + append + str(x) + ".jpg"
             )
         else:
             getpost(
                 url = cardInfo["image_uris"]["normal"],
                 filename = ".temp/" + append + str(x) + ".jpg"
             )
+    return flipCardNums
 
-def stitchImages(deckName, numCards, numTokens):
+def stitchImages(deckName, numCards, numTokens, flipCardNums):
 
     # stitch card images into a 5x5 grid
-    imageNames = [".temp/" + str(num) + ".jpg" for num in range(numCards)]
-    chunks = chunkify(imageNames, 24)
-    for i, chunk in enumerate(chunks):
-        images = [Image.open(imageName) for imageName in chunk]
-        width, height = images[0].size
-        stitchedImage = Image.new("RGB", (5*width, 5*height))
-        for j, image in enumerate(images):
-            stitchedImage.paste(image, (j%5*width, int(j/5)*(height)))
-        stitchedImage.save(".temp/_" + str(i) + ".jpg")
+    stitcher(deckName, list(range(numCards)))
 
     # stitch token images
+    stitcher(deckName, list(range(numTokens)), "t")
 
+    # stitch transform cards
+    if len(flipCardNums):
+        stitcher(deckName, flipCardNums, "tf")
+        stitcher(deckName, flipCardNums, "tb")
 
-def generateJSON(cardData, tokenData):
+def generateJSON(deckName, cardData, cardFrequency, tokenData, flipCardNums, cardBack = "magenta"):
+
+    # main deck
     cardNames = [cardData[x]["name"] for x in range(len(cardData))]
-    return ""
+    cardIDs = [cardIDGen(x) for x in range(len(cardData))]
+
+    chunks = chunkify(cardIDs)
+    numChunks = len(chunks)
+    chunkLengths = [len(chunk) for chunk in chunks]
+    deckImageList = zip(
+        [str(x) for x in range(1, numChunks + 1)],
+        [imageObject(
+            *stitchDimensions(chunkLengths[x]),
+            "images/" + deckName + "_" + str(x) + ".jpg",
+            "assets/" + cardBack + ".jpg"
+        ) for x in range(numChunks)]
+    )
+    deckImage = dict(deckImageList)
+
+    cN = []
+    cIDs = []
+    for name, id, freq in zip(cardNames, cardIDs, cardFrequency):
+        cN += freq * [name]
+        cIDs += freq * [id]
+    cardNames = cN
+    cardIDs = cIDs
+
+    mainDeck = {
+        "Transform": transformObject(posY = 1),
+        "Name": "DeckCustom",
+        "ContainedObjects": [cardObject(name, id) for name, id in zip(cardNames, cardIDs)],
+        "DeckIDs": cardIDs,
+        "CustomDeck": deckImage
+    }
+
+    objectStates = [mainDeck]
+
+    prevDeckNumber = numChunks
+
+    if tokenData:
+        # token deck
+        tokenNames = [tokenData[x]["name"] for x in range(len(tokenData))]
+        tokenIDs = [cardIDGen(x, prevDeckNumber*100) for x in range(len(tokenData))]
+
+        chunks = chunkify(tokenIDs)
+        numChunks = len(chunks)
+        chunkLengths = [len(chunk) for chunk in chunks]
+        tokenImageList = zip(
+            [str(x) for x in range(prevDeckNumber + 1, prevDeckNumber + numChunks + 1)],
+            [imageObject(
+                *stitchDimensions(chunkLengths[x]),
+                "images/" + deckName + "_t" + str(x) + ".jpg",
+                "assets/" + cardBack + ".jpg"
+            ) for x in range(numChunks)]
+        )
+        tokenImage = dict(tokenImageList)
+
+        tokenDeck = {
+            "Transform": transformObject(posY = 1, posX = -4),
+            "Name": "DeckCustom",
+            "ContainedObjects": [cardObject(name, id) for name, id in zip(tokenNames, tokenIDs)],
+            "DeckIDs": tokenIDs,
+            "CustomDeck": tokenImage
+        }
+
+        objectStates += [tokenDeck]
+
+        prevDeckNumber += numChunks
+
+    if flipCardNums:
+        transformNames = [cardData[x]["name"] for x in flipCardNums]
+        transformIDs = [cardIDGen(x, prevDeckNumber*100) for x in range(len(flipCardNums))]
+
+        chunks = chunkify(transformIDs)
+        numChunks = len(chunks)
+        chunkLengths = [len(chunk) for chunk in chunks]
+        frontImageList = zip(
+            [str(x) for x in range(prevDeckNumber + 1, prevDeckNumber + numChunks + 1)],
+            [imageObject(
+                *stitchDimensions(chunkLengths[x]),
+                "images/" + deckName + "_tf" + str(x) + ".jpg",
+                "assets/" + cardBack + ".jpg"
+            ) for x in range(numChunks)]
+        )
+        transformImageList = zip(
+            [str(x) for x in range(prevDeckNumber + 1, prevDeckNumber + numChunks + 1)],
+            [imageObject(
+                *stitchDimensions(chunkLengths[x]),
+                "images/" + deckName + "_tb" + str(x) + ".jpg",
+                "images/" + deckName + "_tb" + str(x) + ".jpg",
+                uniqueBack = True
+            ) for x in range(numChunks)]
+        )
+        transformImage = dict(transformImageList)
+
+        transformDeck = {
+            "Transform": transformObject(posY = 1, posX = -8),
+            "Name": "DeckCustom",
+            "ContainedObjects": [cardObject(name, id) for name, id in zip(transformNames, transformIDs)],
+            "DeckIDs": transformIDs,
+            "CustomDeck": transformImage
+        }
+
+        objectStates += [transformDeck]
+
+    jsonFile = json.dumps(
+        {"ObjectStates": objectStates},
+        indent = 2
+    )
+
+    with open("outputs/" + deckName + ".json", "w+") as file:
+        file.write(jsonFile)
 
 if __name__ == "__main__": main()
